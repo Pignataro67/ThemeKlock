8dcfd27c3e910ecb97a7b26decc4e8db75098339
Pignataro67 <Pignataro67@gmail.com>
2022-07-16 06:43:04 -0700

    adding JS

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+    }
