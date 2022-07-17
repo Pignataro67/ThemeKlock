c5c0a16997930d189a0b0d8d4210a71e094bab4a
Pignataro67 <Pignataro67@gmail.com>
2022-07-17 07:32:39 -0700

    adding JS

--- script.js
+++ script.js
@@ -30,3 +30,13 @@ e.target.innerHTML = 'Dark mode'
     e.target.innerHTML = 'Light mode'
 }
 })
+
+const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
