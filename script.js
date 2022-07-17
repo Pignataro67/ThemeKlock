a42521bb3db7e1255eee9ca36bc646e9bc05f2b2
Pignataro67 <Pignataro67@gmail.com>
2022-07-17 07:30:54 -0700

    adding JS

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
     if (html.classList.contains('dark')) {
